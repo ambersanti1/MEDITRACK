5b62802394559d298bae2eb0c5946a8ecf396096
pgs96 <pau.gs96@gmail.com>
2023-10-24 20:14:21 -0600

    Appointment with Google Maps API

---
+++ Appointments/Appointment_Assets/script.js
@@ -0,0 +1,12 @@
+const date = new Date()
+console.log(date)
+
+let day = date.getDate()
+console.log(day)
+
+let month = date.getMonth() + 1
+console.log(month)
+
+let year = date.getFullYear()
+console.log(year)
+
