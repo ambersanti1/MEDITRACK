5f64b9b377a49b769b72eb8b007bab3c114afe2a
ambersanti <carolinasandoval879@gmail.com>
2023-10-26 16:45:45 -0600

    Add medications

--- js/medications.js
+++ js/medications.js
@@ -4,8 +4,17 @@ $('#currentDay').text(today.format('MMMM D, YYYY'));
 //Scrollable menu
 
 var medicationForm = document.getElementById ("medication-form");
-var display = 0;
 
+
+var addBtn = document.getElementById("add-medicine")
+
+function unshow (){
+  medicationForm.removeAttribute("class", "hide")
+  hideShow()
+}
+addBtn.onclick = unshow;
+
+var display = 1;
 function hideShow() {
 
   if (display == 1) {
@@ -17,13 +26,12 @@ function hideShow() {
   }
 }
 
-var medicineNameEl = document.getElementById("medicine-name");
+var medicineNameEl = document.querySelector(".medicine-name");
 var diseaseEl = document.getElementById("disease");
 var startHourEl = document.getElementById("start-hour");
 var startDateEl = document.getElementById("start-date");
 var frequencyEl = document.getElementById("frequency");
 var durationEl = document.getElementById("duration");
-
 var submitBtn = document.getElementById("submit");
 
 var medicine = document.getElementById("medicine");
@@ -53,45 +61,45 @@ function addTake (event) {
     const addResultsFromStorage = localStorage.getItem("addResult")
     const objectParsed = JSON.parse(addResultsFromStorage)
   }
+  if (medicineName && startHour && startDate && frequency && duration && disease !== ""){
+    event.preventDefault();
+    const course = document.createElement('div');
+    course.setAttribute("class", "course")
+    const preview = document.createElement('div');
+    preview.setAttribute("class", "preview")
+    const elementoPadre = document.querySelector(".container-card")
+    const day = document.createElement("h6")
+    day.setAttribute("id", "day")
+    const hour = document.createElement("h2")
+    const info = document.createElement("div")
+    info.setAttribute("class", "info")
+    const progressWrapper = document.createElement("div")
+    progressWrapper.setAttribute("class", "progress-wrapper")
+    const progress = document.createElement("div")
+    progress.setAttribute("class", "progress")
+    const progressText= document.createElement("span")
+    progressText.setAttribute("class", "progress-text")
+    const newTake= document.createElement("h6")
+    const medicine = document.createElement("h2")
+    medicine.setAttribute("id", "medicine")
+    const discomfort = document.createElement("p")
+    discomfort.setAttribute("id", "discomfort")
+    discomfort.setAttribute("class", "p-trunc")
 
-  event.preventDefault();
-  const course = document.createElement('div');
-  course.setAttribute("class", "course")
-  const preview = document.createElement('div');
-  preview.setAttribute("class", "preview")
-  const elementoPadre = document.querySelector(".container-card")
-  const day = document.createElement("h6")
-  day.setAttribute("id", "day")
-  const hour = document.createElement("h2")
-  const info = document.createElement("div")
-  info.setAttribute("class", "info")
-  const progressWrapper = document.createElement("div")
-  progressWrapper.setAttribute("class", "progress-wrapper")
-  const progress = document.createElement("div")
-  progress.setAttribute("class", "progress")
-  const progressText= document.createElement("span")
-  progressText.setAttribute("class", "progress-text")
-  const newTake= document.createElement("h6")
-  const medicine = document.createElement("h2")
-  medicine.setAttribute("id", "medicine")
-  const discomfort = document.createElement("p")
-  discomfort.setAttribute("id", "discomfort")
-  discomfort.setAttribute("class", "p-trunc")
+    day.textContent = addResult.startDate
+    hour.textContent = addResult.startHour
+    progressText.textContent = "Takes"
+    newTake.textContent = "New take"
+    medicine.textContent = addResult.medicineName
+    discomfort.textContent = addResult.disease
+    elementoPadre.appendChild(course)
+    course.append(preview, info)
+    preview.append(day, hour)
+    info.append(progressWrapper, newTake, medicine, discomfort)
+    progressWrapper.append(progress, progressText)
 
-  day.textContent = addResult.startDate
-  hour.textContent = addResult.startHour
-  progressText.textContent = "Takes"
-  newTake.textContent = "New take"
-  medicine.textContent = addResult.medicineName
-  discomfort.textContent = addResult.disease
-  elementoPadre.appendChild(course)
-  course.append(preview, info)
-  preview.append(day, hour)
-  info.append(progressWrapper, newTake, medicine, discomfort)
-  progressWrapper.append(progress, progressText)
-
-  var formulario = document.getElementById("formulario")
-  formulario.reset()
+    var formulario = document.getElementById("formulario")
+    formulario.reset() 
+  }
 }
 submitBtn.onclick = addTake;
-
